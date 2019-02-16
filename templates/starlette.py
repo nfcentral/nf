1247ea06423699ce261bf1a7d66466a339948765
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2019-02-16 15:26:51 +0300

    Starlette doesn't need prepare

--- templates/starlette.py
+++ templates/starlette.py
@@ -23,7 +23,4 @@ CONFIG_LISTS = {
 
 
 def prepare(config, context):
-    context.update({
-        "app_run_command": json.dumps(["gunicorn", "{}:app".format(context["name"]), "-b", "0.0.0.0:8000", "-k", "uvicorn.workers.UvicornWorker", "-w", "1"]),
-        "app_run_dev_command": json.dumps(["uvicorn", "--host", "0.0.0.0", "--port", "8000", "--debug", "{}:app".format(context["name"])])
-    })
+    pass
